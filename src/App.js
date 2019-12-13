f4f2da4419bbd629a90db5b732e86e36e66a2847
Shashank <s_shashank@outlook.com>
2019-12-13 21:03:11 +0530

    multiple contexts

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Booklist from "./components/Booklist";
 import ThemeContextProvider from "./contexts/ThemeContext";
+import AuthContextProvider from "./contexts/AuthContext";
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <Booklist />
+        <AuthContextProvider>
+          <Navbar />
+          <Booklist />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,73 @@
 import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import { AuthContext } from "../contexts/AuthContext";
 
 class Navbar extends Component {
-  static contextType = ThemeContext;
   render() {
-    const { isLightTheme, navLight, navDark, toggleTheme } = this.context;
-    const theme = isLightTheme ? navLight : navDark;
-    console.log(theme);
     return (
-      <div>
-        <nav className={"navbar navbar-expand-lg " + theme}>
-          <a className="navbar-brand" href="#">
-            BookList Manager
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+      <AuthContext.Consumer>
+        {authContext => (
+          <ThemeContext.Consumer>
+            {context => {
+              const { isAuthenticated, toggleAuth } = authContext;
+              const { isLightTheme, navLight, navDark, toggleTheme } = context;
+              const theme = isLightTheme ? navLight : navDark;
+              return (
+                <div>
+                  <nav className={"navbar navbar-expand-lg " + theme}>
+                    <a className="navbar-brand" href="#">
+                      BookList Manager
+                    </a>
+                    <button
+                      className="navbar-toggler"
+                      type="button"
+                      data-toggle="collapse"
+                      data-target="#navbarSupportedContent"
+                      aria-controls="navbarSupportedContent"
+                      aria-expanded="false"
+                      aria-label="Toggle navigation"
+                    >
+                      <span className="navbar-toggler-icon"></span>
+                    </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  About
-                </a>
-              </li>
-            </ul>
-            <div className="my-2">
-              <button className="btn btn-outline-light" onClick={toggleTheme}>
-                Change Theme
-              </button>
-            </div>
-          </div>
-        </nav>
-      </div>
+                    <div
+                      className="collapse navbar-collapse"
+                      id="navbarSupportedContent"
+                    >
+                      <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                          <a className="nav-link" href="#">
+                            Home
+                          </a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="nav-link" href="#">
+                            About
+                          </a>
+                        </li>
+                      </ul>
+                      <div className="my-2">
+                        <button
+                          className="btn btn-outline-light"
+                          onClick={toggleTheme}
+                        >
+                          Change Theme
+                        </button>
+                        <button
+                          className="btn btn-outline-light ml-2"
+                          onClick={toggleAuth}
+                        >
+                          {isAuthenticated ? "Logout" : "Login"}
+                        </button>
+                      </div>
+                    </div>
+                  </nav>
+                </div>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
