23c4fa176ef74cec30c32dd9780c47bcb5184151
Shashank <s_shashank@outlook.com>
2019-12-14 12:28:15 +0530

    implementing useContext

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddBookForm from "./AddBookForm";
 import uuid from "uuid/v1";
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
-import { AuthContext } from "../contexts/AuthContext";
+import AuthContextProvider, { AuthContext } from "../contexts/AuthContext";
 
-class Navbar extends Component {
+/* class Navbar extends Component {
   render() {
     return (
       <AuthContext.Consumer>
@@ -70,6 +70,57 @@ class Navbar extends Component {
       </AuthContext.Consumer>
     );
   }
-}
+} */
+
+const Navbar = () => {
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+  const { isLightTheme, navLight, navDark, toggleTheme } = useContext(
+    ThemeContext
+  );
+  const theme = isLightTheme ? navLight : navDark;
+  return (
+    <div>
+      <nav className={"navbar navbar-expand-lg " + theme}>
+        <a className="navbar-brand" href="#">
+          BookList Manager
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+          </ul>
+          <div className="my-2">
+            <button className="btn btn-outline-light" onClick={toggleTheme}>
+              Change Theme
+            </button>
+            <button className="btn btn-outline-light ml-2" onClick={toggleAuth}>
+              {isAuthenticated ? "Logout" : "Login"}
+            </button>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
