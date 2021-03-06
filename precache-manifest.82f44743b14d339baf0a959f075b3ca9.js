a8fe09c378ed2a35bab27c5149c8ae97319a779d
Shashank <s_shashank@outlook.com>
2021-03-06 21:09:54 +0530

    Updates

---
+++ precache-manifest.82f44743b14d339baf0a959f075b3ca9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1cdc0557a1c2ee67685fb115c2ed8be",
+    "url": "/BookListApp-ReactHooksAndContext/index.html"
+  },
+  {
+    "revision": "6375a9aa6087343443b2",
+    "url": "/BookListApp-ReactHooksAndContext/static/js/2.ff26f9a8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/BookListApp-ReactHooksAndContext/static/js/2.ff26f9a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e21ea50da2d41a29373",
+    "url": "/BookListApp-ReactHooksAndContext/static/js/main.46f63727.chunk.js"
+  },
+  {
+    "revision": "c9bfbd38e0da5a816e50",
+    "url": "/BookListApp-ReactHooksAndContext/static/js/runtime-main.4bc4a6b8.js"
+  }
+]);
